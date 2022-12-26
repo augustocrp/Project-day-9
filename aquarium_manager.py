35959c9091f9bc3f279f90e2f8c76f9c0b85e861
Augusto Cesar <dev.augusto.crp@gmail.com>
2022-12-25 23:26:02 -0300

    Update aquarium_manager.py

--- aquarium_manager.py
+++ aquarium_manager.py
@@ -9,7 +9,22 @@ def verify_fish(animal):
         return True
     return False
 
-
+#animals_fish = list(filter(lambda animal: (animal["type"] == "fish"), animals))
 
 animals_fish = list(filter(verify_fish, animals))
 print(animals_fish)
+
+def animal_name(animal):
+    return animal["name"]
+
+animals_fish_name = list(map(animal_name, animals_fish))
+print(animals_fish_name)
+
+def assign_to_tank(animals, names_selected, new_tank_number):
+    def change_tank_number(animal):
+        if animal["name"] in names_selected:
+            animal["tank number"] = 42
+        return animal
+    return list(map(change_tank_number, animals))
+
+new_aquarium = assign_to_tank(animals, animals_fish_name, 42)
